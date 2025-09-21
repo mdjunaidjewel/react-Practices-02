dfba2bd1c7d92d60e6364a196d69deef1437650a
mdjunaidjewel <mdjunaidjewell@gmail.com>
2025-09-21 19:26:40 +0600

    some update

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,21 +1,37 @@
 
-import React, { use } from 'react'
+import React, { use, useState } from 'react'
 import Country from '../Country/Country';
 
 export default function Countries({ countriesPromise }) {
 
     const countries = use(countriesPromise);
     const countriesData = countries.countries
-    // console.log(countriesData)
+  // console.log(countriesData)
+  
+  const [visitedCountries, setVisitedCountries] = useState([])
+
+  const handleVisitedCountries = (countryData) => {
+    
+    // console.log("Handle visited Coutry clicked", countryData)
+    const newVisitedCountry = [...visitedCountries, countryData];
+    setVisitedCountries(newVisitedCountry)
+    
+  }
 
     return (
-        <div>
-            <h2 className='text-3xl font-semibold py-4 text-blue-600'>Here Countries: { countriesData.length }</h2>
+      <div>
+        <h2 className="text-3xl font-semibold py-4 text-blue-600">
+          Here Countries: {countriesData.length}
+        </h2>
+        <h3 className="text-2xl font-medium">
+          Visited Countries: {visitedCountries.length}
+        </h3>
         <div className="sm:grid grid-cols-3 gap-5">
           {countriesData.map((countryData) => (
             <Country
               key={countryData.ccn3.ccn3}
               countryData={countryData}
+              handleVisitedCountries={handleVisitedCountries}
             ></Country>
           ))}
         </div>
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,35 +1,48 @@
 import React, { useState } from 'react'
 
-export default function Country({ countryData }) {
-    // console.log(countryData)
-    const { name, population, flags } = countryData;
+export default function Country({ countryData, handleVisitedCountries }) {
+  // console.log(countryData)
+  const { name, population, flags } = countryData;
 
-    const [ visited, setVisited ] = useState(false);
-    
-    const visiteHandler = (countryData) => {
-        // setVisited(true)
-        // 1.System (its Basics System)
-        // if (visited) {
-        //     setVisited(false)
-        // }
-        // else {
-        //     setVisited(true)
-        // }
-        // 2.System
-        // setVisited(visited ? false: true)
+  const [visited, setVisited] = useState(false);
 
-        // 3.System
-        setVisited(!visited)
+  const visiteHandler = () => {
+    // setVisited(true)
+    // 1.System (its Basics System)
+    // if (visited) {
+    //     setVisited(false)
+    // }
+    // else {
+    //     setVisited(true)
+    // }
+    // 2.System
+    // setVisited(visited ? false: true)
 
-    }
+    // 3.System
+      setVisited(!visited);
+      handleVisitedCountries(countryData);
+  };
   return (
-      <div className={`my-5 w-[300px] h-[250px] border-1 p-5 rounded-md shadow-indigo-400 shadow-md space-y-1 ${visited && 'bg-amber-950 text-white'}`} >
-          <img className='w-[200px] h-[100px] mx-auto' src={ flags.flags.png} alt={flags.flags.alt} />
-          <h3>Country: {name.common}</h3>
-          <span className='block'>Population: {population.population}</span>
-          <button className='cursor-pointer p-2 bg-green-600 border-1 border-amber-400 rounded-md text-gray-50 hover:bg-black duration-200'
-              onClick={() => { visiteHandler(countryData) }}
-          >{ visited ? "Visited": "Not Visited" }</button>
+    <div
+      className={`my-5 w-[300px] h-[250px] border-1 p-5 rounded-md shadow-indigo-400 shadow-md space-y-1 ${
+        visited && "bg-amber-950 text-white"
+      }`}
+    >
+      <img
+        className="w-[200px] h-[100px] mx-auto"
+        src={flags.flags.png}
+        alt={flags.flags.alt}
+      />
+      <h3>Country: {name.common}</h3>
+      <span className="block">Population: {population.population}</span>
+      <button
+        className="cursor-pointer p-2 bg-green-600 border-1 border-amber-400 rounded-md text-gray-50 hover:bg-black duration-200"
+        onClick={() => {
+          visiteHandler();
+        }}
+      >
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 }
